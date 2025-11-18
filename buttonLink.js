396855884da2bd435574806c9b0df31543219bb2
justinedasig <jrpdasig@gmail.com>
2025-11-18 23:09:23 +0800

    Add files via upload

---
+++ buttonLink.js
@@ -0,0 +1,29 @@
+document.getElementById('button1')
+.addEventListener('click', function () {
+    window.location.href = "https://justinedasig.github.io/javascript-guessing-game"
+});
+
+document.getElementById('button2')
+.addEventListener('click', function () {
+    window.location.href = "https://justinedasig.github.io/Grade-Evaluation-System"
+})
+
+document.getElementById('button3')
+.addEventListener ('click', function () {
+    window.location.href = "https://justinedasig.github.io/Dasig-Portfolio-Website"
+})
+
+document.getElementById('button4')
+.addEventListener('click', function (){
+    window.location.href = "https://justinedasig.github.io/Javascript-increment-reset-decrement2"
+})
+
+document.getElementById('button5')
+.addEventListener('click', function(){
+    window.location.href = "https://justinedasig.github.io/Lab-Experiment"
+})
+
+document.getElementById('button6')
+.addEventListener('click', function(){
+    window.location.href = "https://justinedasig.github.io/Prelims-lab-3"
+})
